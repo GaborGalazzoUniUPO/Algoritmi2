75efa819b52435137c79da431c67fab7fe4032ee
Gabor Galazzo <gabor.galazzo@star-group.net>
2019-04-15 15:58:07 +0200

    Fix nomi significativi

--- src/uni/gaborgalazzo/algo2/HydraulicConnection.java
+++ src/uni/gaborgalazzo/algo2/HydraulicConnection.java
@@ -1,9 +1,7 @@
 package uni.gaborgalazzo.algo2;
 
-import it.uniupo.algoTools.MST;
 import it.uniupo.algoTools.MinHeap;
 import it.uniupo.graphLib.Edge;
-import it.uniupo.graphLib.GraphInterface;
 import it.uniupo.graphLib.UndirectedGraph;
 
 import java.util.Arrays;
@@ -13,28 +11,26 @@ public class HydraulicConnection {
     private final UndirectedGraph propProject;
     private final UndirectedGraph muniProject;
 
-    private final int connectionPoint;
-    private final int[] propSingleCosts;
-    private int[] muniSingleCosts;
+    private  int[] pipeCostProp;
+    private int[] pipeCostMuni;
     private int digCostProp = 0;
     private int digCostMuni = 0;
 
 
     public HydraulicConnection(UndirectedGraph map, int[][] digCosts, int pipeCost, int connectionPoint) {
 
-        this.connectionPoint = connectionPoint;
 
         muniProject = (UndirectedGraph) map.create();
         boolean[] found = new boolean[map.getOrder()];
         found[connectionPoint] = true;
 
-        propSingleCosts = new int[map.getOrder()];
-        Arrays.fill(propSingleCosts, -1);
-        muniSingleCosts = new int[map.getOrder()];
-        Arrays.fill(muniSingleCosts, -1);
+        pipeCostProp = new int[map.getOrder()];
+        Arrays.fill(pipeCostProp, -1);
+        pipeCostMuni = new int[map.getOrder()];
+        Arrays.fill(pipeCostMuni, -1);
 
-        propSingleCosts[connectionPoint] = 0;
-        muniSingleCosts[connectionPoint] = 0;
+        pipeCostProp[connectionPoint] = 0;
+        pipeCostMuni[connectionPoint] = 0;
 
         MinHeap<Edge, Integer> heap = new MinHeap<>();
         for(Edge e: map.getOutEdges(connectionPoint)){
@@ -46,11 +42,9 @@ public class HydraulicConnection {
             if(!found[e.getHead()]){
                 found[e.getHead()] = true;
                 muniProject.addEdge(new Edge(e.getTail(), e.getHead(), digCosts[e.getHead()][e.getTail()]));
+
                 digCostMuni+=digCosts[e.getHead()][e.getTail()];
-
-
-                propSingleCosts[e.getHead()] = propSingleCosts[e.getTail()] + e.getWeight()*pipeCost;
-
+                pipeCostProp[e.getHead()] = pipeCostProp[e.getTail()] + e.getWeight()*pipeCost;
 
                 for(Edge n: map.getOutEdges(e.getHead()))
                     if(!found[n.getHead()])
@@ -65,20 +59,20 @@ public class HydraulicConnection {
 
         found[connectionPoint] = true;
         for(Edge e: map.getOutEdges(connectionPoint)){
-            heap.add(e, propSingleCosts[connectionPoint] + e.getWeight());
+            heap.add(e, pipeCostProp[connectionPoint] + e.getWeight());
         }
         while (!heap.isEmpty()){
             Edge e = heap.extractMin();
             if(!found[e.getHead()]){
                 found[e.getHead()] = true;
-
-                muniSingleCosts[e.getHead()] = muniSingleCosts[e.getTail()] + e.getWeight()*pipeCost;
-
                 propProject.addEdge(new Edge(e.getTail(), e.getHead(), e.getWeight()*pipeCost));
+
                 digCostProp += digCosts[e.getHead()][e.getTail()];
+                pipeCostMuni[e.getHead()] = pipeCostMuni[e.getTail()] + e.getWeight()*pipeCost;
+
                 for(Edge n: map.getOutEdges(e.getHead()))
                     if(!found[n.getHead()])
-                        heap.add(n, propSingleCosts[n.getTail()] + n.getWeight()*pipeCost);
+                        heap.add(n, pipeCostProp[n.getTail()] + n.getWeight()*pipeCost);
             }
         }
 
@@ -97,8 +91,8 @@ public class HydraulicConnection {
     }
 
     public int extraProp(int place){
-        int propPrjCost = propSingleCosts[place];
-        int muniPrjCost = muniSingleCosts[place];
+        int propPrjCost = pipeCostProp[place];
+        int muniPrjCost = pipeCostMuni[place];
         return propPrjCost-muniPrjCost;
     }
 }
